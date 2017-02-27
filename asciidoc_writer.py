97a493e933481527f1ec43500bc7922c8c83ad0f
Lukas Ruzicka <lruzicka@redhat.com>
2017-02-27 15:38:28 +0100

    Fix table directive, vol 2

--- asciidoc_writer.py
+++ asciidoc_writer.py
@@ -565,7 +565,7 @@ class AsciiDocTranslator(nodes.NodeVisitor):
             clist.append(str(col))
         cline = ','.join(clist)    
         specline = '[cols="'+cline+'",options="header"]\n'
-        introline = "|===\n"
+        introline = "|===\n\h| "
         self.body.append(specline+introline)
 
     def depart_tgroup(self,node):
